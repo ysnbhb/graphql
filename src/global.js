34be01e5f36d888ba85528c1ca3f9c29e4720bb3
ysnbhb <yassine.bahbib@usmba.ac.ma>
2025-02-01 21:37:44 +0100

    login

---
+++ src/global.js
@@ -0,0 +1,16 @@
+export const login = /*html*/`
+    <div class="login-container">
+        <div class="login-card">
+            <h2>Welcome Back</h2>
+            <form   id="form">
+                <div class="input-group">
+                    <input type="text" required placeholder="Enter your email/user name" id="user">
+                </div>
+                <div class="input-group">
+                    <input type="password" required placeholder="Enter your password" id="password">
+                </div>
+                <button type="submit" id="button">Login</button>
+            </form>
+        </div>
+    </div>
+`
---
+++ src/login.js
@@ -0,0 +1,25 @@
+export async function login() {
+  document.getElementById("form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const user = document.getElementById("user");
+    const password = document.getElementById("password");
+    console.log(user.value, password.value);
+
+    const auth = btoa(`${user.value}:${password.value}`);
+    const respons = await fetch("https://learn.zone01oujda.ma/api/auth/signin", {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${auth}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (respons.ok){
+        const data = await respons.json();
+        localStorage.setItem("jwt-token" , data)
+    }else {
+        
+    }
+  });
+}
+
+login();
